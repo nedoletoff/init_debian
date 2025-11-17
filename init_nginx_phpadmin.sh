9bd3d954e4b384da08d85d26653070b74e6ee004
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2025-11-17 10:49:07 +0300

    Update init_nginx_phpadmin.sh
    delete oftware-properties-common for debian 13

--- init_nginx_phpadmin.sh
+++ init_nginx_phpadmin.sh
@@ -70,7 +70,7 @@ apt install -y \
     libbz2-dev libreadline-dev libsqlite3-dev \
     libncursesw5-dev xz-utils tk-dev libxml2-dev \
     libxmlsec1-dev libffi-dev liblzma-dev sysstat \
-    iotop cifs-utils vim expect xclip software-properties-common
+    iotop cifs-utils vim expect xclip 
 check_error "Установка базовых утилит"
 
 echo "Обновление репозиториев перед установкой Nginx..."
